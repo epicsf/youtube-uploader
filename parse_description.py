d3c35d522db6e1f509348877cc6ed2af4a3e00e8
LK Loh <lkloh2410@gmail.com>
2018-09-18 23:07:57 -0700

    add indent

--- parse_description.py
+++ parse_description.py
@@ -6,12 +6,16 @@ python parse_description.py --file="../videos/20180809-284219793/284219793.info.
 '''
 
 def load_file(filename):
-  
+  with open(filename, 'r') as myfile:
+    data = myfile.read()
+    return json.loads(data)
+  return {}
 
 if __name__ == '__main__':
   argparser = argparse.ArgumentParser()
   argparser.add_argument("--file", required=True, help="JSON file to read")
   args = argparser.parse_args()
 
-  d = load_file(args.file);
+  d = load_file(args.file)
+  print json.dumps(d, indent=2)
 
